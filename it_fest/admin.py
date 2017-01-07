26c1c6f444ed971be57dfc70cb1878d9079da153
Safwan Rahman <safwan.rahman15@gmail.com>
2017-01-08 00:28:12 +0600

    Fixing admin

--- it_fest/admin.py
+++ it_fest/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
 from .models import Participant, Registration
 
+
+class ParticipantInline(admin.TabularInline):
+    model = Participant
+
+
+class ParticipationAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'mobile_number',
+                    'institute', 'get_payment_status']
+    list_filter = ['institute']
+    search_fields = ['full_name', 'mobile_number', 'registration_id']
+
+    def get_payment_status(self, object):
+        return object.registration.payment_status
+    get_payment_status.short_description = 'Payment Status'
+
+
+class RegistrationAdmin(admin.ModelAdmin):
+    inlines = [ParticipantInline, ]
+    search_fields = ['id', 'transaction_id', 'participant__mobile_number']
+    list_filter = ['game_name', 'payment_status', ]
+    list_display = ['id', 'payment_status', 'bkash_mobile_number',
+                    'transaction_id', 'game_name', 'get_institute']
+
+    def get_institute(self, object):
+        return object.participant_set.all()[0].institute
+
+    get_institute.short_description = 'Institute'
+
 # Register your models here.
-admin.site.register(Participant)
-admin.site.register(Registration)
+admin.site.register(Participant, ParticipationAdmin)
+admin.site.register(Registration, RegistrationAdmin)
--- registration/settings.py
+++ registration/settings.py
@@ -99,3 +99,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from registration.settings_local import *
+except ImportError:
+    pass
